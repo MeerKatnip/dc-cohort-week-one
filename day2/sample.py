91c8d48685a13c945ae2cae4c135e060530aa2a9
MeerKatnip <amy.roy@icloud.com>
2020-03-31 14:14:56 -0500

    added both sample py files

---
+++ day2/sample.py
@@ -0,0 +1,10 @@
+first_name = input('What is your first name?')
+
+last_name = input('What is your last name?')
+
+# Argument, which goes inside the "()"
+# print('hello world')
+
+# print is a function Called with "()"
+print(first_name)
+print(last_name)
---
+++ day2/sample2.py
@@ -0,0 +1,5 @@
+fav_color = input('What is your favorite color?')
+fav_season = input('What is your favorite season?')
+
+print(fav_color, ' & ', fav_season)
+print('These are both great!')
